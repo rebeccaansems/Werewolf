ff36b106897da0f7d879a85af9828bc5a2dde684
Rebecca Ansems <ansems_rebecca@hotmail.com>
2017-07-22 02:46:21 -0300

    Started clean project
    Clean version of werewolf_old

---
+++ Assets/Scripts/c_JoinGame.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class c_JoinGame : MonoBehaviour
+{
+    public const string VERSION = "0.1";
+
+    public Text errorText;
+    public InputField roomCodeInput;
+
+    private string joinRoomCode;
+    private bool roomExists = false;
+
+    void Start()
+    {
+        PhotonNetwork.autoJoinLobby = true;
+
+        if (!PhotonNetwork.connected)
+        {
+            PhotonNetwork.ConnectUsingSettings(VERSION);
+        }
+    }
+
+    public void JoinGame()
+    {
+        joinRoomCode = roomCodeInput.text.ToUpper();
+
+        RoomInfo[] rooms = PhotonNetwork.GetRoomList();
+
+        for (int i = 0; i < rooms.Length; i++)
+        {
+            if (rooms[i].Name.Equals(joinRoomCode))
+            {
+                Debug.Log("[PHOTON] Joined room: " + joinRoomCode);
+                PhotonNetwork.JoinRoom(joinRoomCode);
+                roomExists = true;
+                break;
+            }
+        }
+
+        if (!roomExists)
+        {
+            Debug.Log("[PHOTON] Failed to join: " + joinRoomCode);
+            errorText.text = "Room code " + joinRoomCode + " does not exist.";
+        }
+    }
+}
---
+++ Assets/Scripts/s_CreateGame.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class s_CreateGame : MonoBehaviour
+{
+    public const string VERSION = "0.1";
+
+    public Text roomCodeText, numberPlayersText;
+
+    public string roomCode { get; private set; }
+
+    private bool playerJoinedRoom = true;
+
+    void Start()
+    {
+        roomCode = getRandomWord();
+        roomCodeText.text = roomCode;
+
+        PhotonNetwork.autoJoinLobby = false;
+
+        if (!(PhotonNetwork.connected))
+        {
+            PhotonNetwork.ConnectUsingSettings(VERSION);
+        }
+    }
+
+    private string getRandomWord()
+    {
+        string possibleLetters = "QWERTYUIOPASDFGHJKLZXCVBNM";
+        string word = "";
+        word += possibleLetters[Random.Range(0, possibleLetters.Length)];
+        word += possibleLetters[Random.Range(0, possibleLetters.Length)];
+        word += possibleLetters[Random.Range(0, possibleLetters.Length)];
+        word += possibleLetters[Random.Range(0, possibleLetters.Length)];
+        word += possibleLetters[Random.Range(0, possibleLetters.Length)];
+        word += possibleLetters[Random.Range(0, possibleLetters.Length)];
+
+        return word;
+    }
+
+    private void Update()
+    {
+        if (PhotonNetwork.connectionStateDetailed.ToString().Equals("ConnectedToMaster") && playerJoinedRoom)
+        {
+            Debug.Log("[PHOTON] Room Created: " + roomCode);
+            PhotonNetwork.JoinOrCreateRoom(roomCode, new RoomOptions() { MaxPlayers = 12, PlayerTtl = 600000 }, TypedLobby.Default);
+
+            playerJoinedRoom = false;
+        }
+
+        if (PhotonNetwork.inRoom)
+        {
+            numberPlayersText.text = PhotonNetwork.room.PlayerCount.ToString();
+        }
+
+    }
+}
