0175ba14dfddfb78c20a6cbd8fd4aae7807cb300
Rebecca Ansems <ansems_rebecca@hotmail.com>
2017-07-24 00:09:42 -0300

    Added some prototype art and ability to select character

--- Assets/Scripts/c_CharacterSelect.cs
+++ Assets/Scripts/c_CharacterSelect.cs
@@ -8,15 +8,23 @@ public class c_CharacterSelect : MonoBehaviour
 
     public InputField playerName;
     public GameObject submitCharPanel, startGamePanel, nameBlockHeader, nameBlock, startGameButton;
+    public Image characterImage;
+    public Sprite[] characters, characterHeadshot;
 
-    private int previousNumPlayers = 1;
+    public string[] characterSubs;
+
+    private int previousNumPlayers = 1, currentCharIndex = 0;
     private List<bool> hasBeenAdded = new List<bool>() { false, false, false, false, false, false };
 
-    public void submitPlayerInfo()
+    public void SubmitPlayerInfo()
     {
         PhotonNetwork.playerName = playerName.text;
         submitCharPanel.SetActive(false);
         startGamePanel.SetActive(true);
+
+        ExitGames.Client.Photon.Hashtable playerInfo = new ExitGames.Client.Photon.Hashtable();
+        playerInfo.Add("CharacterArtNum", currentCharIndex);
+        PhotonNetwork.player.SetCustomProperties(playerInfo);
 
         if (PhotonNetwork.player.ID == 2)
         {
@@ -25,8 +33,25 @@ public class c_CharacterSelect : MonoBehaviour
         }
         else
         {
+            Debug.Log("[PHOTON] Player is not first to join");
             startGameButton.SetActive(false);
         }
+    }
+
+    public void CharacterButtonPressed(int direction)
+    {
+        currentCharIndex += direction;
+
+        if (currentCharIndex < 0)
+        {
+            currentCharIndex = characters.Length - 1;
+        }
+        else if (currentCharIndex > characters.Length - 1)
+        {
+            currentCharIndex = 0;
+        }
+
+        characterImage.sprite = characters[currentCharIndex];
     }
 
     private void Update()
@@ -37,14 +62,18 @@ public class c_CharacterSelect : MonoBehaviour
             {
                 for (int i = 0; i < PhotonNetwork.room.PlayerCount; i++)
                 {
-                    if(PhotonNetwork.playerList[i].CustomProperties["JoinNumber"] != null)
+                    if (PhotonNetwork.playerList[i].CustomProperties["JoinNumber"] != null)
                     {
                         int playerNum = int.Parse(PhotonNetwork.playerList[i].CustomProperties["JoinNumber"].ToString());
                         if (playerNum != -1 && !hasBeenAdded[playerNum] && !PhotonNetwork.playerList[i].NickName.Equals(""))
                         {
                             hasBeenAdded[playerNum] = true;
                             GameObject newNameBlock = Instantiate(nameBlock, nameBlockHeader.transform);
+                            int charIndex = int.Parse(PhotonNetwork.playerList[i].CustomProperties["CharacterArtNum"].ToString());
+
                             newNameBlock.transform.GetChild(0).GetComponent<Text>().text = PhotonNetwork.playerList[i].NickName;
+                            newNameBlock.transform.GetChild(1).GetComponent<Text>().text = characterSubs[charIndex];
+                            newNameBlock.transform.GetChild(2).GetComponent<Image>().sprite = characterHeadshot[charIndex];
                             previousNumPlayers = PhotonNetwork.room.PlayerCount;
                         }
                     }
