76c5c0cb7c3af68668bafca4f57ec5249e77f0cd
Rebecca Ansems <ansems_rebecca@hotmail.com>
2017-07-24 23:20:40 -0300

    Added basics of vibrate vote scene and fixed bugs with players joining

--- Assets/Scripts/c_CharacterSelect.cs
+++ Assets/Scripts/c_CharacterSelect.cs
@@ -1,40 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class c_CharacterSelect : MonoBehaviour
 {
-
     public InputField playerName;
     public GameObject submitCharPanel, startGamePanel, nameBlockHeader, nameBlock, startGameButton;
     public Image characterImage;
-    public Sprite[] characters, characterHeadshot;
-
-    public string[] characterSubs;
 
     private int previousNumPlayers = 1, currentCharIndex = 0;
     private List<bool> hasBeenAdded = new List<bool>() { false, false, false, false, false, false };
 
     public void SubmitPlayerInfo()
     {
-        PhotonNetwork.playerName = playerName.text;
-        submitCharPanel.SetActive(false);
-        startGamePanel.SetActive(true);
-
-        ExitGames.Client.Photon.Hashtable playerInfo = new ExitGames.Client.Photon.Hashtable();
-        playerInfo.Add("CharacterArtNum", currentCharIndex);
-        PhotonNetwork.player.SetCustomProperties(playerInfo);
-
-        if (PhotonNetwork.player.ID == 2)
+        if (!playerName.text.Equals(""))
         {
-            Debug.Log("[PHOTON] Player is first to join");
-            startGameButton.SetActive(true);
-        }
-        else
-        {
-            Debug.Log("[PHOTON] Player is not first to join");
-            startGameButton.SetActive(false);
+            PhotonNetwork.playerName = playerName.text;
+            submitCharPanel.SetActive(false);
+            startGamePanel.SetActive(true);
+
+            ExitGames.Client.Photon.Hashtable playerInfo = new ExitGames.Client.Photon.Hashtable();
+            playerInfo.Add("CharacterArtNum", currentCharIndex);
+            PhotonNetwork.player.SetCustomProperties(playerInfo);
+
+            if (PhotonNetwork.player.ID == 2)
+            {
+                Debug.Log("[PHOTON] Player is first to join");
+                startGameButton.SetActive(true);
+            }
+            else
+            {
+                Debug.Log("[PHOTON] Player is not first to join");
+                startGameButton.SetActive(false);
+            }
         }
     }
 
@@ -44,38 +44,40 @@ public class c_CharacterSelect : MonoBehaviour
 
         if (currentCharIndex < 0)
         {
-            currentCharIndex = characters.Length - 1;
+            currentCharIndex = GetComponent<c_PossibleCharacterInfo>().characters.Length - 1;
         }
-        else if (currentCharIndex > characters.Length - 1)
+        else if (currentCharIndex > GetComponent<c_PossibleCharacterInfo>().characters.Length - 1)
         {
             currentCharIndex = 0;
         }
 
-        characterImage.sprite = characters[currentCharIndex];
+        characterImage.sprite = GetComponent<c_PossibleCharacterInfo>().characters[currentCharIndex];
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(2);
     }
 
     private void Update()
     {
         if (startGamePanel.GetActive())
         {
-            if (PhotonNetwork.inRoom && PhotonNetwork.room.PlayerCount > previousNumPlayers)
+            for (int i = 0; i < PhotonNetwork.room.PlayerCount; i++)
             {
-                for (int i = 0; i < PhotonNetwork.room.PlayerCount; i++)
+                if (PhotonNetwork.playerList[i].CustomProperties["JoinNumber"] != null)
                 {
-                    if (PhotonNetwork.playerList[i].CustomProperties["JoinNumber"] != null)
+                    int playerNum = int.Parse(PhotonNetwork.playerList[i].CustomProperties["JoinNumber"].ToString());
+                    if (playerNum != -1 && !hasBeenAdded[playerNum] && !PhotonNetwork.playerList[i].NickName.Equals(""))
                     {
-                        int playerNum = int.Parse(PhotonNetwork.playerList[i].CustomProperties["JoinNumber"].ToString());
-                        if (playerNum != -1 && !hasBeenAdded[playerNum] && !PhotonNetwork.playerList[i].NickName.Equals(""))
-                        {
-                            hasBeenAdded[playerNum] = true;
-                            GameObject newNameBlock = Instantiate(nameBlock, nameBlockHeader.transform);
-                            int charIndex = int.Parse(PhotonNetwork.playerList[i].CustomProperties["CharacterArtNum"].ToString());
+                        hasBeenAdded[playerNum] = true;
+                        GameObject newNameBlock = Instantiate(nameBlock, nameBlockHeader.transform);
+                        int charIndex = int.Parse(PhotonNetwork.playerList[i].CustomProperties["CharacterArtNum"].ToString());
 
-                            newNameBlock.transform.GetChild(0).GetComponent<Text>().text = PhotonNetwork.playerList[i].NickName;
-                            newNameBlock.transform.GetChild(1).GetComponent<Text>().text = characterSubs[charIndex];
-                            newNameBlock.transform.GetChild(2).GetComponent<Image>().sprite = characterHeadshot[charIndex];
-                            previousNumPlayers = PhotonNetwork.room.PlayerCount;
-                        }
+                        newNameBlock.transform.GetChild(0).GetComponent<Text>().text = PhotonNetwork.playerList[i].NickName;
+                        newNameBlock.transform.GetChild(1).GetComponent<Text>().text = GetComponent<c_PossibleCharacterInfo>().characterSubs[charIndex];
+                        newNameBlock.transform.GetChild(2).GetComponent<Image>().sprite = GetComponent<c_PossibleCharacterInfo>().characterHeadshot[charIndex];
+                        previousNumPlayers = PhotonNetwork.room.PlayerCount;
                     }
                 }
             }
---
+++ Assets/Scripts/c_VoteVibrate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class c_VoteVibrate : MonoBehaviour
+{
+    public GameObject voteBlock, voteBlockHeader;
+
+    void Start()
+    {
+        for (int i = 0; i < PhotonNetwork.room.PlayerCount; i++)
+        {
+            if (PhotonNetwork.playerList[i].CustomProperties["JoinNumber"] != null)
+            {
+                //int playerNum = int.Parse(PhotonNetwork.playerList[i].CustomProperties["JoinNumber"].ToString());
+                //if (playerNum != -1 && !PhotonNetwork.playerList[i].NickName.Equals(""))
+                //{
+                    GameObject newVoteBlock = Instantiate(voteBlock, voteBlockHeader.transform);
+                    int charIndex = int.Parse(PhotonNetwork.playerList[i].CustomProperties["CharacterArtNum"].ToString());
+
+                    voteBlock.transform.GetChild(0).GetComponent<Text>().text = PhotonNetwork.playerList[i].NickName;
+                    voteBlock.transform.GetChild(1).GetComponent<Text>().text = GetComponent<c_PossibleCharacterInfo>().characterSubs[charIndex];
+                    voteBlock.transform.GetChild(2).GetComponent<Image>().sprite = GetComponent<c_PossibleCharacterInfo>().characterHeadshot[charIndex];
+               // }
+            }
+        }
+    }
+}
